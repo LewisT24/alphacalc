c785b2ee4b6591f16524dd31fc9b93b1c68f86e8
Lewis Taylor <lewistaylor24061995@hotmail.co.uk>
2020-10-27 10:05:51 +0000

    Add project files.

---
+++ alphacalc/Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using System;
+
+
+namespace alpha
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize Classes
+            IWebDriver driver = new ChromeDriver();
+            MortgageCostCalculator mcc = new MortgageCostCalculator();
+
+            //All Tests
+            driver.Navigate().GoToUrl("https://www.barclays.co.uk/mortgages/mortgage-calculator/cost-calculator/#/cost");
+
+            //Reason for Mortgage Dropdown
+
+
+            //WebDriverWait wait = new WebDriverWait(driver, 100);
+            //element = wait.until(ExpectedConditions.elementToBeClickable(By.id("mCalc-Reason-ahdg"))); 
+            //SelectElement se = new SelectElement(driver.FindElement(By.Id("mCalc-Reason-ahdg"))); //Locating select list
+            //se.SelectByValue("HP");
+            //
+
+
+            driver.Quit();
+        }
+    }
+
+    public class MortgageCostCalculator
+    {
+        public int Estimatedpropertyvalue;
+        public int BorrowAmount;
+    }
+}
